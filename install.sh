2bd72957ece8858c1386dd70b1ee1ff03b9a60c9
qiansong <2744588786@qq.com>
2024-03-22 15:58:13 +0800

    优化安装脚本，检查安装包是否存在于工作目录，以进行后续操作

--- install.sh
+++ install.sh
@@ -21,6 +21,17 @@ if [[ -f /etc/os-release ]]; then
 	. /etc/os-release
 fi
 
+#######################################
+# 创建配置、插件、安装目录
+# Globals:
+#   none
+# Arguments:
+#   none
+# Outputs:
+#   none
+# Returns:
+#   none
+#######################################
 function create_dir() {
 	# create nvim config dir
 	if [[ ! -d "${nvim_config_dir}" ]]; then
@@ -39,6 +50,17 @@ function create_dir() {
 }
 
 # un tar file in to dir
+#######################################
+# 解压安装包、插件、配置文件等等
+# Globals:
+#   none
+# Arguments:
+#   none
+# Outputs:
+#   输出解压项目的提示信息
+# Returns:
+#   none
+#######################################
 function un_tar_file() {
 	# untar nvim_install file
 	if [[ -d "${nvim_install_dir}/nvim-linux64" ]]; then
@@ -84,6 +106,17 @@ function un_tar_file() {
 }
 
 # define shell env fire VAR
+#######################################
+# 确认用户使用的登录shell类型，获取该shell配置文件
+# Globals:
+#   ${env_file}、${shell_type}、
+# Arguments:
+#   none
+# Outputs:
+#   如果获取失败，将输出错误提示并退出脚本
+# Returns:
+#   none
+#######################################
 function define_shell_env_file() {
 	shell_type="$(echo "${SHELL}" | awk -F '/' '{print $NF}')"
 	case "${shell_type}" in
@@ -101,6 +134,17 @@ function define_shell_env_file() {
 }
 
 # create shellcheck config file
+#######################################
+# 生成shellcheck配置文件
+# Globals:
+#   none
+# Arguments:
+#   none
+# Outputs:
+#   none
+# Returns:
+#   none
+#######################################
 function config_shellcheck_rc_file() {
 
 	if [ ! -f "${HOME}/.shellcheckrc" ]; then
@@ -120,8 +164,68 @@ function config_shellcheck_rc_file() {
 	} >>"${HOME:?}/.shellcheckrc"
 }
 
+# check if installer tar.gz files exist
+#######################################
+# 检测安装包、插件包、配置包是否存在于工作目录
+# Globals:
+#   ${not_found_installer_package[@]}、
+# Arguments:
+#   none
+# Outputs:
+#   none
+# Returns:
+#   0 或 1，0表示包存在，1表示至少有一个包不存在
+#######################################
+function check_installer_tar_gz_files_exist() {
+	local installer_package=()
+	local iterm
+	for iterm in "nvim-config.tar.gz" "nvim-plugin.tar.gz" "nvim-installer.tar.gz"; do
+		installer_package+=("${iterm}")
+	done
+
+	not_found_installer_package=()
+	local package
+	for package in "${installer_package[@]}"; do
+		if [ ! -f "${work_dir}/${package}" ]; then
+			not_found_installer_package+=("${package}")
+		fi
+	done
+
+	if [ "${#not_found_installer_package[@]}" -eq 0 ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
 # install nvim
+#######################################
+# 安装neovim函数
+# Globals:
+#   none
+# Arguments:
+#   none
+# Outputs:
+#   安装成功输出成功信息，反之输出失败信息
+# Returns:
+#   none
+#######################################
 function install_nvim() {
+	if check_installer_tar_gz_files_exist; then
+		true
+	else
+		echo -e "\033[31merror detected!\033[0m installer *tar.gz file NOT FOUND in ${work_dir}"
+		echo -e "-------"
+		local iterm
+		for iterm in "${not_found_installer_package[@]}"; do
+			echo "${iterm}"
+		done
+		echo -e "-------"
+		echo -e "\033[35myou can download them from:\033[0m https://github.com/QianSong1/AstroNvim-neovim-install/releases"
+		echo -e "\033[34mGood Bye!!!\033[0m"
+		print_excuting_msg "Quiting"
+		exit 1
+	fi
 	create_dir
 	un_tar_file
 	define_shell_env_file
@@ -137,8 +241,20 @@ function install_nvim() {
 }
 
 # print excuting msg
+#######################################
+# 打印一个信息输出动画，如退出消息.....
+# Globals:
+#   none
+# Arguments:
+#   传入一个字符串类型消息参数
+# Outputs:
+#   输出一个处理消息动画
+# Returns:
+#   none
+#######################################
 function print_excuting_msg() {
-	message=$1
+	local msg
+	local message="$1"
 	local cahr_1="${message}."
 	local cahr_2="${message}.."
 	local cahr_3="${message}..."
@@ -147,7 +263,7 @@ function print_excuting_msg() {
 	local i=1
 	while [[ "${i}" -le 5 ]]; do
 		r_char="\$cahr_${i}"
-		msg=$(eval "echo -e \"${r_char}\"")
+		msg="$(eval "echo -e \"${r_char}\"")"
 		echo -ne "\033[?25l${msg}\033[0m"
 		echo -ne "\r\r"
 		i=$((i + 1))
@@ -158,6 +274,17 @@ function print_excuting_msg() {
 }
 
 # uninstall nvim
+#######################################
+# 卸载neovim
+# Globals:
+#   none
+# Arguments:
+#   none
+# Outputs:
+#   输出卸载的结果与提示等等
+# Returns:
+#   none
+#######################################
 function uninstall_nvim() {
 	if [[ -d "${nvim_install_dir}/nvim-linux64" ]]; then
 		print_excuting_msg "Uninstalling"
@@ -170,6 +297,7 @@ function uninstall_nvim() {
 		exit
 	else
 		echo -e "\033[1;33mIt seems you have not install neovim yet, do you want to install?\033[0m [y/N]"
+		local you_zl
 		read -rp "> " you_zl
 		while true; do
 			case "${you_zl}" in
@@ -191,6 +319,17 @@ function uninstall_nvim() {
 }
 
 # menu
+#######################################
+# 安装选项菜单
+# Globals:
+#   ${you_zl}、
+# Arguments:
+#   none
+# Outputs:
+#   一个菜单界面
+# Returns:
+#   none
+#######################################
 function select_menu() {
 	echo -e "\033[33mPlease selsct one option to work\033[0m"
 	echo '---------'
@@ -202,6 +341,17 @@ function select_menu() {
 }
 
 # select install or uninstall_nvim
+#######################################
+# 菜单选择函数，选择一个选项工作
+# Globals:
+#   none
+# Arguments:
+#   none
+# Outputs:
+#   none
+# Returns:
+#   none
+#######################################
 function select_option() {
 	clear
 	select_menu
@@ -218,11 +368,38 @@ function select_option() {
 	esac
 }
 
+#######################################
+# 主函数main，程序的入口
+# Globals:
+#   none
+# Arguments:
+#   "$@"
+# Outputs:
+#   none
+# Returns:
+#   none
+#######################################
+function main() {
+	select_option
+}
+
 # exit SIG capture
+#######################################
+# 信号捕捉处理函数，捕捉用户信号作出响应，退出yes|no
+# Globals:
+#   none
+# Arguments:
+#   none
+# Outputs:
+#   输出询问，是否需要退出脚本
+# Returns:
+#   none
+#######################################
 function exit_shell() {
 	echo
 	echo -e "\033[31m\033[1mAre you sure want exit now? [y/N]:\033[0m "
 	echo -n "> "
+	local you_zl
 	read -r you_zl
 	case "${you_zl}" in
 	y | Y | yes)
@@ -245,4 +422,4 @@ for i in HUP INT QUIT TSTP; do
 	eval "${trap_cmd}"
 done
 
-select_option
+main "@"
